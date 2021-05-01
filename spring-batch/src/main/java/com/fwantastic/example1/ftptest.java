57f21e64a017c1b87e1722747a15935fe445d77a
KimGuenho <kimkgh8@gmail.com>
2021-05-01 20:36:39 +0900

    예제

--- spring-batch/src/main/java/com/fwantastic/example1/ftptest.java
+++ spring-batch/src/main/java/com/fwantastic/example1/ftptest.java
@@ -1,93 +1,70 @@
 package com.fwantastic.example1;
 
-import java.io.File;
-import java.util.Map;
+import java.io.IOException;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
-import com.spire.xls.FileFormat;
-import com.spire.xls.Workbook;
-
 import common.commonUtils;
 
 /**
- * PDF변환 처리
- * 엑셀파일을 PDF파일로 변환하는 배치프로그램
- * 프로퍼티파일에 지정된 경로의 폴더 안의 파일이 대상
+ * PDF변환 처리 엑셀파일을 PDF파일로 변환하는 배치프로그램 프로퍼티파일에 지정된 경로의 폴더 안의 파일이 대상
  */
 public class ftptest implements Tasklet {
-    public RepeatStatus execute(final StepContribution contribution, final ChunkContext chunkContext) throws Exception {
-        commonUtils.outLog(ftptest.class, "INFO", "PDF변환 처리 작업 시작");
-        try {
-        	Map<String, String> propertyMap = commonUtils.getProperties("ExcelToPdf.properties");
-        	
-        	//대상디렉토리
-        	File excelDir = new File(propertyMap.get("inputFilePath"));
-        	File[] excelFiles = excelDir.listFiles();
-        	
-        	for (File targetFile:excelFiles) {
-        		 // 확장자
-        		 String extension = targetFile.getName().substring(targetFile.getName().lastIndexOf("."),targetFile.getName().length());
-        		 // 확장자를 제거한 파일명
-        		 String fileName = targetFile.getName().substring(0,targetFile.getName().lastIndexOf("."));
-        		 
-        		 Workbook wb = new Workbook();
-        		 wb.loadFromFile(targetFile.getPath());
-        		 //PDF로 저장함
-        	     wb.saveToFile(propertyMap.get("outFilePath") + fileName+".pdf",FileFormat.PDF);
-        		 
-        		 /* 다른 방법
-                 XSSFWorkbook xls_workbook = new XSSFWorkbook(input_excel);
-                 
-                 XSSFSheet worksheet = xls_workbook.getSheetAt(0);
+	public RepeatStatus execute(final StepContribution contribution, final ChunkContext chunkContext) throws Exception {
+		commonUtils.outLog(ftptest.class, "INFO", "PDF변환 처리 작업 시작");
+		FTPClient ftpClient = null;
+		try {
+			ftpClient = new FTPClient();
+			ftpClient.setControlEncoding("euc-kr"); // 한글파일명 때문에 디폴트 인코딩을 euc-kr로 합니다
+			ftpClient.connect("user.chollian.net"); // 천리안 FTP에 접속합니다
 
-                 Iterator<Row> rowIterator = worksheet.iterator();
-                 
-                 Document ouput_pdf = new Document();
-                 PdfWriter.getInstance(ouput_pdf, new FileOutputStream(propertyMap.get("outFilePath") + fileName+".pdf"));
-                 ouput_pdf.open();
-                 
-                 PdfPTable my_table = new PdfPTable(2);
+			int reply = ftpClient.getReplyCode(); // 응답코드가 비정상이면 종료합니다
+			if (!FTPReply.isPositiveCompletion(reply)) {
+				ftpClient.disconnect();
+				System.out.println("FTP server refused connection.");
+			} else {
+				System.out.print(ftpClient.getReplyString()); // 응답 메세지를 찍어봅시다
 
-                 PdfPCell table_cell;
-                 
-                 while (rowIterator.hasNext()) {
-                 	Row row = rowIterator.next();
-                 	Iterator<Cell> cellIterator = row.cellIterator();
-                 	while (cellIterator.hasNext()) {
-                 		Cell cell = cellIterator.next();
-                 		switch (cell.getCellType()) {
-                 		case Cell.CELL_TYPE_STRING:
-                 			table_cell=new PdfPCell(new Phrase(cell.getStringCellValue()));
-                 			my_table.addCell(table_cell);
-                 			break;
-                 		}
-                 	}
+				ftpClient.setSoTimeout(10000); // 현재 커넥션 timeout을 millisecond 값으로 입력합니다
+				ftpClient.login("TEST", "PASSWORD"); // 로그인 유저명과 비밀번호를 입력 합니다
 
-                 }
-                 ouput_pdf.add(my_table);
-                 ouput_pdf.close();  
-                 input_excel.close();
-                 */
-        		 
-        		 
-        		 
-                 //File renameFile = new File(propertyMap.get("backupPath")+targetFile.getName()+".xlsx");
-                 //targetFile.renameTo(renameFile);
-        	}
-            
-        } catch(Exception e) {
-        	System.out.println(e.getMessage());
-        	e.printStackTrace();
-        }
-        
-        commonUtils.outLog(ftptest.class, "INFO", "PDF변환 처리 작업 완료");
-        return null;
-    }
+				// 목록보기 구현
 
+				FTPFile[] ftpfiles = ftpClient.listFiles("/public"); // public 폴더의 모든 파일을 list 합니다
+				if (ftpfiles != null) {
+					for (int i = 0; i < ftpfiles.length; i++) {
+						FTPFile file = ftpfiles[i];
+						System.out.println(file.toString()); // file.getName(), file.getSize() 등등..
+					}
+				}
+
+				ftpClient.logout();
+			}
+
+		} catch (Exception e) {
+			System.out.println(e);
+			e.printStackTrace();
+		} finally {
+			if (ftpClient != null && ftpClient.isConnected()) {
+				try {
+					ftpClient.disconnect();
+					commonUtils.outLog(ftptest.class, "INFO", "PDF변환 처리 작업 완료");
+					return null;
+				} catch (IOException ioe) {
+					ioe.printStackTrace();
+					return null;
+					
+				}
+			} else {
+				return null;
+			}
+			
+		}
+	}
 }
